35ea8c08a716114afdcd7a6f750d6be1aa36799f
HelenSwan <127235143+HelenSwan@users.noreply.github.com>
2024-03-18 11:19:31 +0700

    Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,33 +1,37 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { db } from "../firebase";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useUser } from './UserContext';
+import {db} from '../firebase';
+//import { collection, getDocs, query, where } from 'firebase/firestore'; // Add this import statement
 import { getDoc, doc } from 'firebase/firestore';
-import "./css/Login.css";
 
 function Login() {
   const navigate = useNavigate();
+  const { setUsername } = useUser();
   const [inputs, setInputs] = useState({});
 
   const handleChange = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
+    setInputs((values) => ({...values, [name]: value}));
+  }
+ 
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     if (!inputs.username || !inputs.password) {
-      console.log("Please enter username or password");
-      alert("Please enter username or password");
+      console.log('Please enter username or password');
+      alert('Please enter username or password');
       return;
     }
 
-   const { username, password } = inputs;
+  const { username, password } = inputs;
   try {
     const userDocRef = doc(db, 'account', username);
     const userDocSnapshot = await getDoc(userDocRef);
 
+    
     if (!userDocSnapshot.exists()) {
       console.log('User not found');
       alert('Invalid username or password');
@@ -38,7 +42,9 @@ function Login() {
 
     if (userData.password === password) {
       console.log('Login successful!');
-      navigate('/Home');
+      console.log('User:', username); 
+      setUsername(inputs.username);
+      navigate(`/Home`);
     } else {
       console.log('Invalid password');
       alert('Invalid username or password');
@@ -50,38 +56,38 @@ function Login() {
   }
 };
 
-
   return (
-    <form onSubmit={handleSubmit} className="Form-Login">
-      <div className="div-parent">
-        <h1>Login</h1>
-        <div className="input-box">
-          username
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={inputs.username || ""}
-            onChange={handleChange}
-          />
-        </div>
 
-        <div className="password-box">
-          password
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={inputs.password || ""}
-            onChange={handleChange}
-          />
-        </div>
+    <form onSubmit= {handleSubmit}>
+      <p>username</p>
+      <div className="input-box">
+        <input
+          type = "text"
+          id = "username"
+          name="username"
+          value = {inputs.username || ''}
+          onChange= {handleChange}
+        />
+      </div>
 
-        <br />
+      <p>password</p>
 
-        <div>
-          <button type="submit" className="Login-Button">Login</button>
-        </div>
+      <div className="password-box">
+        <input
+          type = "password"
+          id = "password"
+          name="password"
+          value = {inputs.password || ''}
+          onChange = {handleChange}
+        />
+      </div>
+
+      <br />
+
+      <div>
+        <button type="submit" >
+          Login
+        </button>
       </div>
     </form>
   );
