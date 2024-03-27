aee97c5ccc699fce26a2321784886653c8041c76
HelenSwan <127235143+HelenSwan@users.noreply.github.com>
2024-03-27 21:31:56 +0700

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,25 @@
 import Navbar from "../components/Navbar";
+import React from 'react';
+import { Link, useParams  } from 'react-router-dom';
+import { useUser } from './UserContext';
 
 function Home(){
+   const { username } = useUser();
+   console.log("Username in Home:", username);
    return (
-
       <div>
-         <Navbar/>
-         <h1>Home page</h1>
-      </div>
-      
+         <Navbar />
+         <h1>Home page, {username}</h1>
+ 
+       <br />
+       <Link to={`/quotation`}>
+        <button>Quotation</button>
+      </Link>
+      <br />
+      <Link to={`/product`}>
+        <button>Product</button>
+      </Link>
+       </div>
    );
 }
 
