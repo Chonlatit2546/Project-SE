257a3d6595b47cfe72eea63ac4a5c2ffaebdfa46
HelenSwan <127235143+HelenSwan@users.noreply.github.com>
2024-03-28 11:24:37 +0700

    Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,53 +7,61 @@ import "../App.css";
 import "./css/Navbar.css";
 import { IconContext } from "react-icons";
 
-function Navbar() {
-  const [sidebar, setSidebar] = useState(false);
-  const [activeLink, setActiveLink] = useState("");
+function Navbar({ setMenuActive }) {
+   const [sidebar, setSidebar] = useState(true);
+   const [activeLink, setActiveLink] = useState("");
 
-  const toggleSidebar = () => setSidebar(!sidebar);
-  const handleItemClick = (title) => {
-    setActiveLink(title);
-    toggleSidebar();
-  };
+   const toggleSidebar = () => {
+      setSidebar(!sidebar);
+      setMenuActive(!sidebar);
+   };
+   const handleItemClick = (title) => {
+      setActiveLink(title);
+      toggleSidebar();
+      setMenuActive(!sidebar);
+   };
 
-  return (
-    <>
-      <IconContext.Provider value={{ color: "undefined" }}>
-        <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-          <ul className="nav-menu-items">
-            <li className="navbar-toggle">
-              <div className="supply-pro-container">
-                <div className="supply-pro">
-                  <span className="supply">Supply</span>
-                  <span className="pro">Pro</span>
-                  <Link to="#" className="menu-bars" onClick={toggleSidebar}>
-                    {sidebar ? (
-                      <Mdicons.MdOutlineArrowBackIos />
-                    ) : (
-                      <Mdicons.MdOutlineArrowForwardIos />
-                    )}
-                  </Link>
-                </div>
-              </div>
-            </li>
-            {SidebarData.map((item, index) => (
-              <li key={index} className={item.cName}>
-                <Link
-                  to={item.path}
-                  className={item.title === activeLink ? "active" : ""}
-                  onClick={() => handleItemClick(item.title)}
-                >
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </IconContext.Provider>
-    </>
-  );
+
+   return (
+      <>
+         <IconContext.Provider value={{ color: "undefined" }}>
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+               <ul className="nav-menu-items">
+                  <li className="navbar-toggle">
+                     <div className="supply-pro-container">
+                        <div className="supply-pro">
+                           <span className="supply">Supply</span>
+                           <span className="pro">Pro</span>
+                           <Link to="#" className="menu-bars" onClick={toggleSidebar}>
+                              {sidebar ? (
+                                 <Mdicons.MdOutlineArrowBackIos />
+                              ) : (
+                                 <Mdicons.MdOutlineArrowForwardIos />
+                              )}
+                           </Link>
+                        </div>
+                     </div>
+                     <div className="other-container">
+                        {/* Content for the other container */}
+                     </div>
+                  </li>
+                  {SidebarData.map((item, index) => (
+                     <li key={index} className={item.cName}>
+                        <Link
+                           to={item.path}
+                           className={item.title === activeLink ? "active" : ""}
+                           onClick={() => handleItemClick(item.title)}
+                        >
+                           {item.icon}
+                           <span>{item.title}</span>
+                        </Link>
+                     </li>
+                  ))}
+               </ul>
+            </nav>
+         </IconContext.Provider>
+      </>
+   );
 }
 
 export default Navbar;
