81de5a8a1b407de9b7c183d9654844c89d3c8168
HelenSwan <127235143+HelenSwan@users.noreply.github.com>
2024-03-30 00:35:08 +0700

    Add files via upload

---
+++ src/pages/PurchaseOrderPDF.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
+
+// Create styles
+const styles = StyleSheet.create({
+    page: {
+      flexDirection: 'column',
+      padding: 10,
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+    tableRow: {
+      flexDirection: 'row',
+      borderBottomWidth: 1,
+      borderColor: '#000',
+      paddingVertical: 5,
+    },
+    tableCell: {
+        flex: 1,
+        textAlign: 'left',
+        marginLeft: 8,
+        fontSize: 10,
+        color: '#333',
+      },
+    qtyCell: {
+      flex: 1,
+      textAlign: 'center',
+      marginLeft: 8,
+      fontSize: 10,
+      color: '#333',
+    },
+    itemCell: {
+        flex: 1,
+      textAlign: 'left',
+      marginLeft: 8,
+      fontSize: 10,
+      color: '#333',
+    },
+    descriptionCell: {
+        flex: 5, 
+        textAlign: 'left',
+        fontSize: 10,
+        color: '#333',
+      },
+    headerText: {
+      fontWeight: 'bold',
+      fontSize: 12,
+      marginBottom: 5,
+    },
+  });
+
+  const PurchaseOrderPDF = ({ purchaseOrderID, purchaseOrderData, quotationData, productData, productPOData, total, vat, grandTotal }) => (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text style={styles.headerText}>Purchase Order No: {purchaseOrderID}      Issued Date: {purchaseOrderData.issuedDate}      Expired Date: {purchaseOrderData.expiredDate}</Text>
+          <Text style={styles.headerText}>Customer Name: {quotationData.cusName}</Text>
+          <Text style={styles.headerText}>Customer Address: {quotationData.cusAddress || "-"}</Text>
+          <Text style={styles.headerText}>Customer Email: {quotationData.cusEmail || "-"}</Text>
+          <Text style={styles.headerText}>Customer Department: {quotationData.cusDepartment || "-"}           Customer Phone Number: {quotationData.cusPhoneNo}</Text>
+          <Text style={[styles.headerText, { marginTop: 10 }]}>        Item  |                                 Description                                |   Qty   |   Unit   |  UnitPrice  |    Amount       </Text>
+          {productData.map((product, index) => (
+            <View style={styles.tableRow} key={index}>
+              <Text style={styles.itemCell}>{index + 1}</Text>
+              <Text style={styles.descriptionCell}>{product.productName} ({product.color}) Material: {product.material}</Text>
+              <Text style={styles.qtyCell}>{productPOData[`productNo${index + 1}`].quantity}</Text>
+              <Text style={styles.tableCell}>{product.unit}</Text>
+              <Text style={styles.tableCell}>{productPOData[`productNo${index + 1}`].unitPrice}</Text>
+              <Text style={styles.tableCell}>{(productPOData[`productNo${index + 1}`].quantity * productPOData[`productNo${index + 1}`].unitPrice).toFixed(2)}</Text>
+            </View>
+          ))}
+          <Text style={[styles.headerText, { marginTop: 10 }]}>-----Summary-----</Text>< br/>
+          <Text style={styles.headerText}>Total: {total} </Text><Text style={styles.headerText}>VAT (7%): {vat}</Text>
+          <Text style={styles.headerText}>Grand Total: {grandTotal}</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+  
+  export default PurchaseOrderPDF;
